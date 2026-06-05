0542f1b6987e62dbcd66a89478020f4024300b01
Sandeep Pandit <54panditsandeep@gmail.com>
2026-06-05 18:31:55 +0000

    QA promotion changes added in repo

---
+++ scripts/deploy-qa.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+IMAGE_TAG=$1
+
+kubectl set image deployment/ironclad-app \
+ironclad-app=sandy541998/ironclad-app:${IMAGE_TAG} \
+-n qa
+
+kubectl rollout status deployment/ironclad-app -n qa
